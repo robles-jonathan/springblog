342a761234f074d8721c1ec76436f60bc62e5db5
Jonathan Robles <jonathan.robles287@gmail.com>
2022-02-22 15:02:54 -0600

    Completed PostController.

---
+++ src/main/java/com/codeup/springblog/PostController.java
@@ -0,0 +1,35 @@
+package com.codeup.springblog;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class PostController {
+    @GetMapping("/posts")
+    @ResponseBody
+    public String index(){
+        return "Post index page";
+    }
+
+    @GetMapping("/posts/{id}")
+    @ResponseBody
+    public String viewPost(@PathVariable int id){
+        return "This is the individual post with the id of: " + id + ".";
+    }
+
+    @GetMapping("/posts/create")
+    @ResponseBody
+    public String create(){
+        return "This is the create form.";
+    }
+
+    @PostMapping("/posts/create")
+    @ResponseBody
+    public String createPost(){
+        return "Post Created";
+    }
+}
+
