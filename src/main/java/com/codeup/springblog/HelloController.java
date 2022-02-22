b48dd6df3d18af280c91fc1a371797f37e6f13fd
Jonathan Robles <jonathan.robles287@gmail.com>
2022-02-22 12:22:33 -0600

    Finsihed Controllers lecture.

---
+++ src/main/java/com/codeup/springblog/HelloController.java
@@ -0,0 +1,26 @@
+package com.codeup.springblog;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class HelloController {
+    @GetMapping("/hello/{name}")
+    @ResponseBody
+    public String hello(@PathVariable("name") String name){
+        return "Hello, " + name + "!";
+    }
+
+    @GetMapping("/test")
+    @ResponseBody
+    public String test(){
+        return "test";
+    }
+
+    //REQUEST MAPPING
+    @RequestMapping(path = "increment/{number}", method = RequestMethod.GET)
+    @ResponseBody
+    public String increment(@PathVariable int number){
+        return number + " plus one is " + (number + 1) + "!";
+    }
+}
