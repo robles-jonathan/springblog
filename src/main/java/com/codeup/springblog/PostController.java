950d07e893bdc1d85780ad2a52d742f4cc55114a
Jonathan Robles <jonathan.robles287@gmail.com>
2022-02-23 12:32:09 -0600

    Completed exercise.

--- src/main/java/com/codeup/springblog/PostController.java
+++ src/main/java/com/codeup/springblog/PostController.java
@@ -1,23 +1,33 @@
 package com.codeup.springblog;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 public class PostController {
     @GetMapping("/posts")
-    @ResponseBody
-    public String index(){
-        return "Post index page";
+    public String index(Model model){
+        Post postOne = new Post("Post One Title", "This is the body of Post One.");
+        Post postTwo = new Post("Post Two Title", "This is the body of Post Two.");
+        List<Post> posts = new ArrayList<>();
+        posts.add(postOne);
+        posts.add(postTwo);
+        model.addAttribute("posts", posts);
+        return "posts/index";
     }
 
     @GetMapping("/posts/{id}")
-    @ResponseBody
-    public String viewPost(@PathVariable long id){
-        return "This is the individual post with the id of: " + id + ".";
+        public String viewPost(@PathVariable long id, Model model){
+        Post postOne = new Post("title", "body description");
+        model.addAttribute("post", postOne);
+        return "posts/show";
     }
 
     @GetMapping("/posts/create")
