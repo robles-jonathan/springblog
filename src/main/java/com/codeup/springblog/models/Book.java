135307444e61a0d0ba443b7d48a4e82a776f54bf
Jonathan Robles <jonathan.robles287@gmail.com>
2022-02-24 15:56:42 -0600

    Spring JPA LEC.

---
+++ src/main/java/com/codeup/springblog/models/Book.java
@@ -0,0 +1,50 @@
+package com.codeup.springblog.models;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "books")
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false, length = 100)
+    private String Title;
+
+    @Column(nullable = false)
+    private String author;
+
+    public Book() {
+    }
+
+    public Book(long id, String title, String author) {
+        this.id = id;
+        Title = title;
+        this.author = author;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return Title;
+    }
+
+    public void setTitle(String title) {
+        Title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+}
