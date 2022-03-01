af667c7298778b037050d39e6c994f4c1dd9fe94
Jonathan Robles <jonathan.robles287@gmail.com>
2022-03-01 16:35:57 -0600

    Created EmailService and implemented it in the create post, to send the creator of the post an email.

--- src/main/java/com/codeup/springblog/controllers/PostController.java
+++ src/main/java/com/codeup/springblog/controllers/PostController.java
@@ -4,6 +4,7 @@ import com.codeup.springblog.models.Post;
 import com.codeup.springblog.models.User;
 import com.codeup.springblog.repositories.PostRepository;
 import com.codeup.springblog.repositories.UserRepository;
+import com.codeup.springblog.services.EmailService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -13,10 +14,12 @@ import org.springframework.web.bind.annotation.*;
 public class PostController {
     private PostRepository postDao;
     private UserRepository userDao;
+    private final EmailService emailService;
 
-    public PostController(PostRepository postDao, UserRepository userDao) {
+    public PostController(PostRepository postDao, UserRepository userDao, EmailService emailService) {
         this.postDao = postDao;
         this.userDao = userDao;
+        this.emailService = emailService;
     }
 
     @GetMapping("/posts")
@@ -42,8 +45,15 @@ public class PostController {
     public String createPost(@ModelAttribute Post post){
         User currentUser = userDao.getById(1L);
         post.setUser(currentUser);
+        emailService.prepareAndSend(post, post.getTitle(), post.getBody());
         postDao.save(post);
         return "redirect:/posts";
+    }
+
+    @GetMapping("/send-email")
+    public String sendEmail() {
+        emailService.prepareAndSend("Testing", "Did this work");
+        return "redirect:/";
     }
 
     @GetMapping("/posts/{id}/edit")
