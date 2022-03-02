45e311c6780de7633c227e6a06ace5947a495212
Jonathan Robles <jonathan.robles287@gmail.com>
2022-03-02 09:55:22 -0600

    Security Configuration

--- src/main/java/com/codeup/springblog/models/User.java
+++ src/main/java/com/codeup/springblog/models/User.java
@@ -40,6 +40,14 @@ public class User {
         this.posts = posts;
     }
 
+    public User(User copy) {
+        id = copy.id; // This line is SUPER important! Many things won't work if it's absent
+        email = copy.email;
+        username = copy.username;
+        password = copy.password;
+        posts = copy.posts;
+    }
+
     public long getId() {
         return id;
     }
